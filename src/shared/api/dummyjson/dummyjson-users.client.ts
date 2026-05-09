f55689b6f3fb6be986713c405d34fb1f4cdc16f1
tolya <tolya.sorokin2017@gmail.com>
2026-05-09 12:01:04 +0700

    feat: define dummyjson user field whitelist

---
+++ src/shared/api/dummyjson/dummyjson-users.client.ts
@@ -0,0 +1,14 @@
+export const USERS_SELECT_FIELDS = [
+  'id',
+  'firstName',
+  'lastName',
+  'age',
+  'gender',
+  'email',
+  'phone',
+  'image',
+  'company',
+  'role',
+  'address',
+  'university',
+] as const
